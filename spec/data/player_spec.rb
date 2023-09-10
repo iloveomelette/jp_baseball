3bfd7aa8bed949763f60c4a67ac4f1f139ebc446
iloveomelette <mizuki1998hosei@gmail.com>
2023-09-10 14:55:51 +0900

    refactor(data_spec): apply modification by rubocop

--- spec/data/player_spec.rb
+++ spec/data/player_spec.rb
@@ -1,25 +1,39 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
 require 'yaml'
 
-RSpec.describe Data do
+RSpec.describe 'Data' do
   describe 'player.yml' do
-    let!(:list_of_players) { YAML.load_file(File.join(File.dirname(__FILE__),
-                                                      '../../data/players.yml')) }
-
-    it 'has correct leagues' do
-      leagues = list_of_players.map(&:keys).flatten
-      expect(leagues.size).to eq 2
-      expect(leagues).to include(:central, :pacific)
+    let!(:list_of_players) do
+      YAML.load_file(File.join(File.dirname(__FILE__),
+                               '../../data/players.yml'))
     end
 
-    it 'has correct teams' do
-      central_teams = list_of_players.first[:central].map(&:keys).flatten
-      pacific_teams = list_of_players.last[:pacific].map(&:keys).flatten
-      expect(central_teams.size).to eq 6
-      expect(central_teams).to include(:swallows, :baystars, :carp, :dragons, :giants, :tigers)
-      expect(pacific_teams.size).to eq 6
-      expect(pacific_teams).to include(:baffaloes, :fighters, :hawks, :lions, :marines, :eagles)
+    it 'has correct size of leagues' do
+      expect(list_of_players.map(&:keys).flatten.size).to eq 2
+    end
+
+    it 'has correct leagues' do
+      expect(list_of_players.map(&:keys).flatten).to include(:central, :pacific)
+    end
+
+    it 'has correct size of teams in central' do
+      expect(list_of_players.first[:central].map(&:keys).flatten.size).to eq 6
+    end
+
+    it 'has correct teams in central' do
+      teams = list_of_players.first[:central].map(&:keys).flatten
+      expect(teams).to include(:swallows, :baystars, :carp, :dragons, :giants, :tigers)
+    end
+
+    it 'has correct size of teams in pacific' do
+      expect(list_of_players.last[:pacific].map(&:keys).flatten.size).to eq 6
+    end
+
+    it 'has correct teams in pacific' do
+      teams = list_of_players.last[:pacific].map(&:keys).flatten
+      expect(teams).to include(:baffaloes, :fighters, :hawks, :lions, :marines, :eagles)
     end
   end
 end
